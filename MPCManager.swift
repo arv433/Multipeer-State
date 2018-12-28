53436a07a96a1da1fb6d2df5a9f719f354f0c78f
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-12-28 10:19:57 -0500

    Touched up model controllers and other minor fixes

--- MPCManager.swift
+++ MPCManager.swift
@@ -10,155 +10,90 @@ import Foundation
 import MultipeerConnectivity
 
 class MPCManager: NSObject {
-    var advertiser: MCNearbyServiceAdvertiser
-    var browser: MCNearbyServiceBrowser
-    
-    static let instance = MPCManager()
-    
-    let localPeerID: MCPeerID
-    let serviceType = "MPC-Testing"
-    
-    var devices: [Device] = []
-    
-    override init() {
-        // Get archived peerID if it exists or create a new one in the device's name and store it
-        if let data = UserDefaults.standard.data(forKey: "peerID"), let id = try? NSKeyedUnarchiver.unarchivedObject(ofClass: MCPeerID.self, from: data) {
-            self.localPeerID = id!
+	var advertiser: MCNearbyServiceAdvertiser!
+	var browser: MCNearbyServiceBrowser!
+
+	struct Notifications {
+		static let deviceDidChangeState = Notification.Name("deviceDidChangeState")
+	}
+	
+	static let instance = MPCManager()
+	
+	let localPeerID: MCPeerID
+	let serviceType = "MPC-Testing"
+	
+	var devices: [Device] = []
+	
+	
+	override init() {
+        if let data = UserDefaults.standard.data(forKey: "peerID"), let id = NSKeyedUnarchiver.unarchiveObject(with: data) as? MCPeerID {
+            self.localPeerID = id
         } else {
             let peerID = MCPeerID(displayName: UIDevice.current.name)
-            let data = try? NSKeyedArchiver.archivedData(withRootObject: peerID, requiringSecureCoding: false)
+            let data = NSKeyedArchiver.archivedData(withRootObject: peerID)
             UserDefaults.standard.set(data, forKey: "peerID")
             self.localPeerID = peerID
         }
-
-        self.advertiser = MCNearbyServiceAdvertiser(peer: localPeerID, discoveryInfo: nil, serviceType: self.serviceType)
-        self.browser = MCNearbyServiceBrowser(peer: localPeerID, serviceType: self.serviceType)
         
         super.init()
-
+        
+        self.advertiser = MCNearbyServiceAdvertiser(peer: localPeerID, discoveryInfo: nil, serviceType: self.serviceType)
         self.advertiser.delegate = self
+        
+        self.browser = MCNearbyServiceBrowser(peer: localPeerID, serviceType: self.serviceType)
         self.browser.delegate = self
-    }
-    
-    func device(for id: MCPeerID) -> Device {
-        // If the device is already in the devices property, return it
-        for device in devices {
-            if device.peerID == id { return device }
-        }
-        
-        // Otherwise create a new device with the given ID
-        let device = Device(peerID: id)
-        
-        // Append to the devices property and return it
-        devices.append(device)
-        return device
-    }
-    
-    func start() {
-        self.advertiser.startAdvertisingPeer()
-        self.browser.startBrowsingForPeers()
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(enteredBackground), name: UIApplication.didEnterBackgroundNotification, object: nil)
-    }
+	}
+	
+	func device(for id: MCPeerID) -> Device {
+		for device in self.devices {
+			if device.peerID == id { return device }
+		}
+		
+		let device = Device(peerID: id)
+		
+		self.devices.append(device)
+		return device
+	}
 
-    @objc func enteredBackground() {
-        for device in self.devices {
-            device.disconnect()
-        }
-    }
-
-    struct Notifications {
-        static let deviceDidChangeState = Notification.Name("deviceDidChangeState")
-    }
-}
-
-
-extension MPCManager: MCNearbyServiceAdvertiserDelegate {
-    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
-        let device = MPCManager.instance.device(for: peerID)
-        device.connect()
-        invitationHandler(true, device.session)
-    }
-}
-
-extension MPCManager: MCNearbyServiceBrowserDelegate {
-    func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
-        let device = MPCManager.instance.device(for: peerID)
-        device.invite()
-    }
-
-    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {}
-
+	func start() {
+		self.advertiser.startAdvertisingPeer()
+		self.browser.startBrowsingForPeers()
+		
+		NotificationCenter.default.addObserver(self, selector: #selector(enteredBackground), name: UIApplication.didEnterBackgroundNotification, object: nil)
+	}
+	
+	@objc func enteredBackground() {
+		for device in self.devices {
+			device.disconnect()
+		}
+	}
 }
 
 extension MPCManager {
-    var connectedDevices: [Device] {
-        return self.devices.filter{ $0.state == .connected }
-    }
+	var connectedDevices: [Device] {
+		return self.devices.filter { $0.state == .connected }
+	}
+}
+
+extension MPCManager: MCNearbyServiceAdvertiserDelegate {
+	func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+		let device = MPCManager.instance.device(for: peerID)
+		device.connect()
+		invitationHandler(true, device.session)
+		//  Handle our incoming peer
+	}
+}
+
+extension MPCManager: MCNearbyServiceBrowserDelegate {
+	func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
+		let device = MPCManager.instance.device(for: peerID)
+		device.invite(with: self.browser)
+	}
+	
+	func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+		let device = MPCManager.instance.device(for: peerID)
+		device.disconnect()
+	}
 }
 
 
-// Class for invidual Devices
-class Device: NSObject {
-    let peerID: MCPeerID
-    var session: MCSession?
-    var name: String
-    var state = MCSessionState.notConnected
-    var lastMessageRecieved: Message?
-    
-    init(peerID: MCPeerID) {
-        self.name = peerID.displayName
-        self.peerID = peerID
-        super.init()
-    }
-    
-    func invite() {
-        MPCManager.instance.browser.invitePeer(self.peerID, to: self.session!, withContext: nil, timeout: 10)
-    }
-    
-    func connect() {
-        // Do not run if there already exists a session
-        if self.session != nil { return }
-        
-        self.session = MCSession(peer: MPCManager.instance.localPeerID, securityIdentity: nil, encryptionPreference: .required)
-        self.session?.delegate = self
-    }
-    
-    func disconnect() {
-        self.session?.disconnect()
-        self.session = nil
-    }
-}
-
-extension Device: MCSessionDelegate {
-    static let messageRecievedNotification = Notification.Name("DeviceDidRecieveMessage")
-
-    public func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
-        self.state = state
-        NotificationCenter.default.post(name: MPCManager.Notifications.deviceDidChangeState, object: self)
-    }
-    
-    public func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
-        if let message = try? JSONDecoder().decode(Message.self, from: data) {
-            lastMessageRecieved = message
-            NotificationCenter.default.post(name: Device.messageRecievedNotification, object: message, userInfo: ["from": self])
-        }
-    }
-
-    public func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {}
-    public func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {}
-    public func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {}
-}
-
-extension Device {
-    func send(text: String) throws {
-        let message = Message(body: text)
-        let payload = try JSONEncoder().encode(message)
-        try self.session?.send(payload, toPeers: [self.peerID], with: .reliable)
-    }
-}
-
-// Message structure containing body
-struct Message: Codable {
-    let body: String
-}
